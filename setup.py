d453c5fd8399fea3045430b2925376ac091e2072
OctavianLee <octavianlee1@gmail.com>
2015-08-10 01:54:27 +0800

    Modify the structure of setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+Cytisas is a suit for python project. It have will realize serveral libraries in
+Python by myself.
+"""
+
 from setuptools import setup, find_packages
 
-url="https://github.com/OctavianLee/Cytisas"
+URL = "https://github.com/OctavianLee/Cytisas"
 VERSION = "0.0.0"
 
 setup(
     name="cytisas",
     version=VERSION,
     license='MIT',
-    description="A suite for python project.",
     author="Octavian Lee",
     author_email="octavianlee1@gmail.com",
-    url=url,
-    packages=find_packages(),
+    url=URL,
+    description="A suite for python project.",
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
 )
